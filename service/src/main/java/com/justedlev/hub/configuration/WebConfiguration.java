1428dd501519a680513444ec9091b15280006f8c
Edward L. <59051661+Justedlev@users.noreply.github.com>
2023-10-08 20:22:46 +0300

    -

---
+++ service/src/main/java/com/justedlev/hub/configuration/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.justedlev.hub.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*");
+    }
+}
